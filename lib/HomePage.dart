1362ecf87488b6672032fea54c52f814fc4c8692
liyoucheng <geeklyc@163.com>
2020-03-31 22:08:48 +0800

    【新增】异步处理

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,17 +1,96 @@
 import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterdemo/MallPage.dart';
+import 'dart:isolate';
+import 'dart:async';
+
 
 class HomePage extends StatefulWidget {
   State<StatefulWidget> createState() => _HomePage();
 }
 
+
+
 class _HomePage extends State<HomePage> {
   String _msg = '';
+
+//  Future<String> fetchContent() =>
+//      Future<String>.delayed(Duration(seconds:3), () => "Hello")
+//          .then((x) => "$x 2019");
+
+  //声明了一个延迟2秒返回Hello的Future，并注册了一个then返回拼接后的Hello 2019
+  Future<String> fetchContent() =>
+      Future<String>.delayed(Duration(seconds:2), () => "Hello")
+          .then((x) => "$x 2019");
+//异步函数会同步等待Hello 2019的返回，并打印
+  func() async => print(await fetchContent());
+
   @override
   Widget build(BuildContext context) {
-    print(formatDate(DateTime.now(), [mm, '月', dd, '日', hh, ':', n]));
-    print(formatDate(DateTime.now(), [m, '月第', w, '周']));
+//    print(formatDate(DateTime.now(), [mm, '月', dd, '日', hh, ':', n]));
+//    print(formatDate(DateTime.now(), [m, '月第', w, '周']));
+
+
+//    Future(() => print('Running in Future 1'));//下一个事件循环输出字符串
+//
+//    Future(() => print('Running in Future 2'))
+//        .then((_) => print('and then 1'))
+//        .then((_) => print('and then 2'));
+//        //上一个事件循环结束后，连续输出三段字符串
+
+
+////f1比f2先执行
+//    Future(() => print('f1'));
+//    Future(() => print('f2'));
+//
+////f3执行后会立刻同步执行then 3
+//    Future(() => print('f3')).then((_) => print('then 3'));
+//
+////then 4会加入微任务队列，尽快执行
+//    Future(() => null).then((_) => print('then 4'));
+
+//    Future(() => print('f1'));//声明一个匿名Future
+//    Future fx = Future(() =>  null);//声明Future fx，其执行体为null
+//
+////声明一个匿名Future，并注册了两个then。在第一个then回调里启动了一个微任务
+//    Future(() => print('f2')).then((_) {
+//      print('f3');
+//      scheduleMicrotask(() => print('f4'));
+//    }).then((_) => print('f5'));
+//
+////声明了一个匿名Future，并注册了两个then。第一个then是一个Future
+//    Future(() => print('f6'))
+//        .then((_) => Future(() => print('f7')))
+//        .then((_) => print('f8'));
+//
+////声明了一个匿名Future
+//    Future(() => print('f9'));
+//
+////往执行体为null的fx注册了了一个then
+//    fx.then((_) => print('f10'));
+//
+////启动一个微任务
+//    scheduleMicrotask(() => print('f11'));
+//    print('f12');
+
+
+//声明了一个延迟3秒返回Hello的Future，并注册了一个then返回拼接后的Hello 2019
+
+//    print(fetchContent());
+
+
+//    Future(() => print('f1'))
+//        .then((_) async => await Future(() => print('f2')))
+//        .then((_) => print('f3'));
+//    Future(() => print('f4'));
+
+
+
+
+    print("func before");
+    func();
+    print("func after");
+
     return new Scaffold(
       appBar: AppBar(
         title: Text('新首页'),
